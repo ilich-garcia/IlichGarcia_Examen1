bc0522338a87a660d8fc6fb24ca00a1de3bdb43f
Ilich García <ilich-garcia@unitec.edu>
2017-02-17 15:45:36 -0600

    Terminando NobleFamily.cpp

--- Dothraki.cpp
+++ Dothraki.cpp
@@ -14,6 +14,52 @@ Dothraki::setName(string name) {
 	this -> name = name;
 }
 
+Dothraki::setChief(string chief) {
+   this -> chief = chief;
+}
+
+Dothraki::setHorse(string horse) {
+   this -> horse = horse;
+}
+
+Dothraki::setColor(string color) {
+   this -> color = color;
+}
+
+Dothraki::setAttack(int attack) {
+   this -> attack = attack;
+}
+
+Dothraki::setDefense(int defense) {
+   this -> defense = defense;
+}
+
+// Getters.
+
+string Dothraki::getName() {
+   return name;
+}
+
+string Dothraki::getChief() {
+   return chief;
+}
+
+string Dothraki::getHorse() {
+   return horse;
+}
+
+string Dothraki::getColor() {
+   return color;
+}
+
+int Dothraki::getAttack() {
+   return attack;
+}
+
+int Dothraki::getDefense() {
+   return defense;
+}
+
 // Destructor.
 
 Dothraki::~Dothraki() {
